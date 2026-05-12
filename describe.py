ef85ffc47bd6f84d718cd67a2033f8dffa99bd26
Noan <noan.pujol@gmail.com>
2026-05-12 17:23:50 +0200

    historgram started

--- describe.py
+++ describe.py
@@ -7,8 +7,9 @@ def count(data, column):
     i = 0
     for row in data[column]:
         if not math.isnan(row):
-            i+=1
+            i += 1
     return float(i)
+
 
 def mean(data, column):
     i = 0
@@ -16,20 +17,19 @@ def mean(data, column):
     for row in data[column]:
         if math.isnan(row):
             continue
-        i+=1
+        i += 1
         summ += float(row)
-    return float(summ/i)
+    return float(summ / i)
 
 
 def std(data, column, mean):
     summ = 0
-    i = 0
     for row in data[column]:
         if math.isnan(row):
             continue
         summ += (row - mean) ** 2
-        i = 0
-    return float((summ / 1) ** 0.5)  #sqrt
+    return float((summ / 1) ** 0.5)  # sqrt
+
 
 def min_and_max(data, column):
     min = data[column][0]
@@ -43,16 +43,39 @@ def min_and_max(data, column):
             max = row
     return (float(min), float(max))
 
-def quicksort(rows):
-    return sorted(rows)
-    
+
+def ft_len(element):
+    j = 0
+    for _ in element:
+        j += 1
+    return j
+
+
+def quicksort(rows, left, right):
+    if right <= left:
+        return
+    pivot = rows[(left + right) // 2]
+    l, r = left, right
+    while l <= r:
+        while rows[l] < pivot:
+            l += 1
+        while rows[r] > pivot:
+            r -= 1
+        if l <= r:
+            rows[l], rows[r] = rows[r], rows[l]
+            l += 1
+            r -= 1
+    quicksort(rows, left, r)
+    quicksort(rows, l, right)
+    return rows
+
 
 def calulate_25_50_75(data, column, count):
     c25 = None
     c50 = None
     c75 = None
     i = 0
-    sorted_rows = quicksort(data[column])
+    sorted_rows = quicksort(data[column].copy(), 0, ft_len(data[column]) - 1)
     for row in sorted_rows:
         if math.isnan(row):
             continue
@@ -60,26 +83,26 @@ def calulate_25_50_75(data, column, count):
             c25 = row
         elif not c50 and i == int(count * 0.5):
             c50 = row
-        elif i == int (count * 0.75):
+        elif i == int(count * 0.75):
             c75 = row
             break
-        i+=1
-    return c25,c50,c75
-    
+        i += 1
+    return c25, c50, c75
 
-def create_stats(data : pd.DataFrame):
+
+def create_stats(data: pd.DataFrame):
     stats = {
-        "Count":[],
-        "Mean":[],
-        "Std":[], # Standard Deviation
-        "Min":[],
-        "25%":[],
-        "50%":[], 
-        "75%":[],
-        "Max":[]
+        "Count": [],
+        "Mean": [],
+        "Std": [],  # Standard Deviation
+        "Min": [],
+        "25%": [],
+        "50%": [],
+        "75%": [],
+        "Max": []
     }
-    
-    for key,value in stats.items():
+
+    for key, value in stats.items():
         i = 0
         if key == "50%":
             break
@@ -97,28 +120,21 @@ def create_stats(data : pd.DataFrame):
                 stats["Min"].append(min)
                 stats["Max"].append(max)
             elif key == "25%":
-                c25,c50,c75 = calulate_25_50_75(data, column, stats["Count"][i])
+                c25, c50, c75 = calulate_25_50_75(data,
+                                                  column, stats["Count"][i])
                 stats["25%"] = c25
                 stats["50%"] = c50
                 stats["75%"] = c75
-                
-                
-            i+=1
-
-
+            i += 1
     df = pd.DataFrame.from_dict(stats)
-    df.index = [f"Feature {i}" for i in range(len(stats["Count"]))]
+    df.index = [f"Feature {i}" for i in range(1, len(stats["Count"]) + 1)]
     print(df.T)
-                
-            
-            
-    
-    
+
 
 def main():
     try:
         argv = sys.argv
-        assert len(argv) == 2, "Wrong number of arguments"    
+        assert len(argv) == 2, "Wrong number of arguments"
         data = pd.read_csv(argv[1])
         create_stats(data)
     except AssertionError as e:
---
+++ histogram.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def iterate_on_rows(data: pd.DataFrame):
+    means = data.groupby("Hogwarts House").mean(numeric_only=True)
+    std = means.std()
+    mean = means.mean()
+    # cv = std/mean
+    print(means)
+    minn = std.idxmin()
+    print("min : ", minn)
+    # print(cv)
+
+    Hufflepuff = data[data["Hogwarts House"] == "Hufflepuff"][minn]
+    Ravenclaw = data[data["Hogwarts House"] == "Ravenclaw"][minn]
+    Gryffindor = data[data["Hogwarts House"] == "Gryffindor"][minn]
+    Slytherin = data[data["Hogwarts House"] == "Slytherin"][minn]
+    plt.hist(Hufflepuff, bins=20)
+    plt.hist(Ravenclaw, bins=20)
+    plt.hist(Gryffindor, bins=20)
+    plt.hist(Slytherin, bins=20)
+    plt.xlabel(f"Grades of {minn}")
+    plt.ylabel("Number of students")
+    plt.title(f"Distribution of grades: {minn}")
+    plt.show()
+    # cv.plot()
+    # plt.hist(cv)
+    # plt.show()
+
+                    
+                
+                
+
+
+def main():
+    # try:
+    data = pd.read_csv("datasets/dataset_train.csv")
+    iterate_on_rows(data)
+    # except Exception as  e:
+    #     print(e)
+    #     exit(1)
+
+    return
+
+
+if __name__ == "__main__":
+    main()
